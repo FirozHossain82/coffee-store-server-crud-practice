494535193e37c9f3535fddec32b1ab2096e506fd
FirozHossain82 <firozhossain295@gmail.com>
2023-10-23 11:04:35 +0600

    updated

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
     })
 
 
+    // update the single product
+    app.get('/coffee/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+    
 
     // create post ---------POST---------Notun data create hobe database  e
     app.post('/coffee', async(req, res) =>{
